8eef584a573dbc8cad540424cd24a6ea81b81ea3
todyTech <y0unn@hanmail.net>
2018-11-06 16:15:22 +0900

    feat: Update controller for go to access denied page (1)

--- securityPrj/src/main/java/tody/common/controller/LoginController.java
+++ securityPrj/src/main/java/tody/common/controller/LoginController.java
@@ -2,6 +2,7 @@ package tody.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
@@ -9,6 +10,11 @@ public class LoginController {
 	@RequestMapping(value="/secu/loginPage")
 	public String page() throws Exception {
 		return "/secu/loginPage";
+	}
+	
+	@RequestMapping(value="/access_denied_page")
+	public String accessDeniedPage() throws Exception {
+		return "/access_denied_page";
 	}
 
 }
