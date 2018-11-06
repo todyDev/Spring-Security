f1b05d2a6de655e4b5a9cd0926a2dfdcbdad3fa4
todyTech <y0unn@hanmail.net>
2018-11-06 16:20:57 +0900

    feat: Update controller for go to access denied page (2)

--- securityPrj/src/main/java/tody/common/controller/LoginController.java
+++ securityPrj/src/main/java/tody/common/controller/LoginController.java
@@ -16,5 +16,13 @@ public class LoginController {
 	public String accessDeniedPage() throws Exception {
 		return "/access_denied_page";
 	}
+	
+    @RequestMapping(value="/access_denied")
+    public ModelAndView accessDenied() throws Exception {
+    	ModelAndView mv = new ModelAndView("/goIndex");
+    	mv.addObject("msg", "접근 권한이 없습니다.");
+    	mv.addObject("url", "/");
+    	return mv;
+    }
 
 }
