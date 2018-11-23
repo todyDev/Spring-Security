4ea4ba1233bd1e3dba0703e35ec6bef673081175
todyDev <y0unn@hanmail.net>
2018-11-23 15:45:46 +0900

    feat: Add CustomUserDetails to store user information

---
+++ securityPrj/src/main/java/tody/common/vo/CustomUserDetails.java
@@ -0,0 +1,64 @@
+package tody.common.vo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@SuppressWarnings("serial")
+public class CustomUserDetails implements UserDetails {
+	
+	private String ID;
+	private String PASSWORD;
+	private String AUTHORITY;
+	private boolean ENABLED;
+	private String NAME;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		ArrayList<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+		auth.add(new SimpleGrantedAuthority(AUTHORITY));
+		return auth;
+	}
+
+	@Override
+	public String getPassword() {
+		return PASSWORD;
+	}
+
+	@Override
+	public String getUsername() {
+		return ID;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return ENABLED;
+	}
+	
+	public String getNAME() {
+		return NAME;
+	}
+
+	public void setNAME(String name) {
+		NAME = name;
+	}
+
+}
