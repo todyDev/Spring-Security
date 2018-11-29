10026ece520b96ec8f8ff7ac0c7873e9045257d4
todyDev <y0unn@hanmail.net>
2018-11-29 18:11:55 +0900

    fix #3: Add check password method

--- securityPrj/src/main/java/tody/common/service/CustomAuthenticationProvider.java
+++ securityPrj/src/main/java/tody/common/service/CustomAuthenticationProvider.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -30,12 +31,20 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		CustomUserDetails user = (CustomUserDetails) userDeSer.loadUserByUsername(username);
 		Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) user.getAuthorities();
 		
+		if(!matchPassword(username, user.getPassword())) {
+			throw new BadCredentialsException(username);
+		}
+		
 		return new UsernamePasswordAuthenticationToken(username, password, authorities);
 	}
 
 	@Override
 	public boolean supports(Class<?> authentication) {
 		return true;
+	}
+	
+	private boolean matchPassword(String loginPwd, String password) {
+		return loginPwd.equals(password);
 	}
 
 }
