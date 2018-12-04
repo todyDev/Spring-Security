4d762e4957657b5be7c4c30be8c1ead5d682bb5f
todyDev <y0unn@hanmail.net>
2018-12-04 18:16:48 +0900

    feat: Add update failure count

--- securityPrj/src/main/java/tody/common/handler/LoginFailureHandler.java
+++ securityPrj/src/main/java/tody/common/handler/LoginFailureHandler.java
@@ -6,14 +6,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+import tody.common.service.CustomUserDetailsService;
+
 public class LoginFailureHandler implements AuthenticationFailureHandler {
+	
+	@Autowired
+	private CustomUserDetailsService userDeSer;
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
 			throws IOException, ServletException {
+		
+		String loginId = request.getParameter("loginId");
+
+		userDeSer.countFailure(loginId);
 		
 		request.setAttribute("ID", request.getParameter("loginId"));
 		request.setAttribute("PASSWORD", request.getParameter("loginPwd"));
