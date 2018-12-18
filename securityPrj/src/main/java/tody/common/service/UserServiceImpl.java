83c88a2bbb934c05f8bc3a1b37b1f28c419cef1a
todyDev <y0unn@hanmail.net>
2018-12-18 18:06:54 +0900

    feat: Modify and Add Service and DAO

--- securityPrj/src/main/java/tody/common/service/UserServiceImpl.java
+++ securityPrj/src/main/java/tody/common/service/UserServiceImpl.java
@@ -1,8 +1,30 @@
 package tody.common.service;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
-@Service("usr")
+import tody.common.dao.UserAuthDAO;
+
+@Service("userSer")
 public class UserServiceImpl implements UserService {
+	
+	@Resource(name="userAuthDAO")
+	private UserAuthDAO userDAO;
+
+	@Override
+	public void countFailure(String username) {
+		userDAO.updateFailureCount(username);
+	}
+
+	@Override
+	public int checkFailureCount(String username) {
+		return userDAO.checkFailureCount(username);
+	}
+
+	@Override
+	public void disabledUsername(String username) {
+		userDAO.updateDisabled(username);
+	}
 
 }
