6f33b4948f9a863802c0db56509264fcd10b9a1f
todyDev <y0unn@hanmail.net>
2018-12-24 16:58:57 +0900

    feat: Add LoginSuccessHandler

---
+++ securityPrj/src/main/java/tody/common/handler/LoginSuccessHandler.java
@@ -0,0 +1,21 @@
+package tody.common.handler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
