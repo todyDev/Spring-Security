a346ec835f9e273e524a07fcfdeeb1550bd370dd
todyDev <y0unn@hanmail.net>
2018-12-26 16:09:03 +0900

    feat: Add redirect page set to move after successful login

--- securityPrj/src/main/java/tody/common/handler/LoginSuccessHandler.java
+++ securityPrj/src/main/java/tody/common/handler/LoginSuccessHandler.java
@@ -5,17 +5,76 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+	
+	Logger log = Logger.getLogger(getClass());
+	
+	private RequestCache requestCache = new HttpSessionRequestCache();
+	private RedirectStrategy redirectStratgy = new DefaultRedirectStrategy();
+	
+	private String defaultUrl;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		
+		//에러 세션 지우기
+		clearAuthenticationAttributes(request);
+		
+		//Redirect URL 작업
+		resultRedirectStrategy(request, response, authentication);
+		
+	}
+	
+	protected void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if(session==null) return;
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	}
+	
+	protected void resultRedirectStrategy(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
+		
+		if(savedRequest!=null) {
+			log.debug("권한이 필요한 페이지에 접근했을 경우");
+			useSessionUrl(request, response);
+		} else {
+			log.debug("직접 로그인 url로 이동했을 경우");
+			useDefaultUrl(request, response);
+		}
+		
+	}
+	
+	protected void useSessionUrl(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
+		String targetUrl = savedRequest.getRedirectUrl();
+		redirectStratgy.sendRedirect(request, response, targetUrl);
+	}
+	
+	protected void useDefaultUrl(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		redirectStratgy.sendRedirect(request, response, defaultUrl);
+	}
 
+	public String getDefaultUrl() {
+		return defaultUrl;
+	}
+
+	public void setDefaultUrl(String defaultUrl) {
+		this.defaultUrl = defaultUrl;
 	}
 
 }
