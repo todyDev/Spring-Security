59009fd0693f5968e44470ed4c840d9582a6dc24
todyDev <y0unn@hanmail.net>
2018-12-28 14:59:34 +0900

    feat: Add exception message set

--- securityPrj/src/main/java/tody/common/handler/LoginFailureHandler.java
+++ securityPrj/src/main/java/tody/common/handler/LoginFailureHandler.java
@@ -6,8 +6,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import tody.common.util.MessageUtils;
 
 public class LoginFailureHandler implements AuthenticationFailureHandler {
 
@@ -15,9 +21,21 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
 			throws IOException, ServletException {
 		
+		String errormsg = null;
+		
+		if(exception instanceof BadCredentialsException) {
+			errormsg = MessageUtils.getMessage("error.BadCredentials");
+		} else if(exception instanceof InternalAuthenticationServiceException) {
+			errormsg = MessageUtils.getMessage("error.BadCredentials");
+		} else if(exception instanceof DisabledException) {
+			errormsg = MessageUtils.getMessage("error.Disaled");
+		} else if(exception instanceof CredentialsExpiredException) {
+			errormsg = MessageUtils.getMessage("error.CredentialsExpired");
+		}
+		
 		request.setAttribute("ID", request.getParameter("loginId"));
 		request.setAttribute("PASSWORD", request.getParameter("loginPwd"));
-		request.setAttribute("ERRORMSG",exception.getMessage());
+		request.setAttribute("ERRORMSG", errormsg);
 		request.getRequestDispatcher("/secu/loginPage?error").forward(request, response);
 	}
 
