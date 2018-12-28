1d0327db6575ce913dee1ae251e5e1c0e91c45af
todyDev <y0unn@hanmail.net>
2018-12-28 15:06:07 +0900

    feat: Add property set in login failure handler

--- securityPrj/src/main/java/tody/common/handler/LoginFailureHandler.java
+++ securityPrj/src/main/java/tody/common/handler/LoginFailureHandler.java
@@ -16,6 +16,11 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import tody.common.util.MessageUtils;
 
 public class LoginFailureHandler implements AuthenticationFailureHandler {
+	
+	private String loginidname;
+	private String loginpwdname;
+	private String errormsgname;
+	private String defaultFailureUrl;
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
@@ -37,6 +42,38 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
 		request.setAttribute("PASSWORD", request.getParameter("loginPwd"));
 		request.setAttribute("ERRORMSG", errormsg);
 		request.getRequestDispatcher("/secu/loginPage?error").forward(request, response);
+	}
+
+	public String getLoginidname() {
+		return loginidname;
+	}
+
+	public void setLoginidname(String loginidname) {
+		this.loginidname = loginidname;
+	}
+
+	public String getLoginpwdname() {
+		return loginpwdname;
+	}
+
+	public void setLoginpwdname(String loginpwdname) {
+		this.loginpwdname = loginpwdname;
+	}
+
+	public String getErrormsgname() {
+		return errormsgname;
+	}
+
+	public void setErrormsgname(String errormsgname) {
+		this.errormsgname = errormsgname;
+	}
+
+	public String getDefaultFailureUrl() {
+		return defaultFailureUrl;
+	}
+
+	public void setDefaultFailureUrl(String defaultFailureUrl) {
+		this.defaultFailureUrl = defaultFailureUrl;
 	}
 
 }
